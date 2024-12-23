52068320cb5a093a2dc5acd569d47feb4a42623c
troyrhodes02 <wtrhodes02@gmail.com>
2024-12-23 02:04:25 -0600

    Navbar and Hero Animation Update

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from "react";
+import { Stack, useMediaQuery, useTheme } from "@mui/material";
+import { NavbarMobile } from "./NavbarMobile";
+import { NavbarDesktop } from "./NavbarDesktop";
+import { useRouter } from "next/navigation";
+
+export const Navbar = () => {
+  const router = useRouter();
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+
+  return (
+    <Stack>
+      {isMobile ? (
+        <NavbarMobile />
+      ) : (
+        <NavbarDesktop/>
+      )}
+    </Stack>
+  );
+};
