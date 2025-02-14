f1fce1092d1755d39d92053bf67de3bd627ef707
troyrhodes02 <wtrhodes02@gmail.com>
2025-02-14 00:33:09 -0600

    changes

--- components/sections/cases/CaseStudiesDesktop.tsx
+++ components/sections/cases/CaseStudiesDesktop.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Box, Typography, Grid, Card, Button, CardActionArea } from "@mui/material";
+import { Box, Typography, Grid, Button } from "@mui/material";
 import { motion } from "framer-motion";
 
 export const mockProjects = [
